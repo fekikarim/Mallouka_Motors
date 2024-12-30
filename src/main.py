20730678188435142c89c0be2d28e21e3ecd833e
fekikarim <feki.karim@esprit.tn>
2024-12-30 02:42:00 +0100

    Update logo path in MainApp initialization

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from flet import *
 from MainApp import MainApp
 
 def main(page: Page):
-    MainApp(page, "Allo Casse Auto", "./allo_casse_auto_logo.jpg")
+    MainApp(page, "Allo Casse Auto", "./logo/mallouka_motors_logo.png")
     page.update()
 
 app(target=main, assets_dir="assets")
