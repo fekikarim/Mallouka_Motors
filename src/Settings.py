6fac830bbc92615a5eadd21888624ae288823d8a
fekikarim <feki.karim@esprit.tn>
2024-12-30 02:42:17 +0100

    Add about dialog and theme switcher to settings view

--- src/Settings.py
+++ src/Settings.py
@@ -18,35 +18,142 @@ class Settings:
         self.ub = AppBarMaster(self.page, self.parent_class, self.themer, self.page_name, self.icon)
         self.upperbar = self.ub.app_bar_frame
         
+        self.switcher = Container(
+                content=Column(
+                    controls=[
+                        self.parent_class.themer.theme_switch,
+                    ],
+                    horizontal_alignment=CrossAxisAlignment.CENTER,
+                ),
+                margin=margin.only(top=20, bottom=20),
+                alignment=alignment.center,
+            )
+        
+        self.page.bottom_appbar = BottomAppBar(
+            shape=NotchShape.CIRCULAR,
+            content=Container(
+                content=Text(
+                    "Developed by ",
+                    spans=[
+                        TextSpan(
+                            "Karim Feki",
+                            url="https://www.linkedin.com/in/karimfeki/",
+                            style=TextStyle(
+                                decoration=TextDecoration.UNDERLINE,
+                                color=colors.BLUE_ACCENT_700,
+                            ),
+                        ),
+                        TextSpan(" ©"),
+                    ],
+                ),
+                expand=True,
+                alignment=alignment.center,
+            ),
+            expand=True,
+        )
+        
         self.settings_frame = self.make_frame()
         
         self.controls = [
             self.settings_frame,
             self.upperbar,
         ]
-
-    def make_frame(self):
         
-        self.settings_frame = SafeArea(
+    def open_about_dialog(self, e):
+        dlg = AlertDialog(
+            content=Column(
+                [
+                    Row(
+                        [
+                            Text("Mallouka Motors", size=20, weight=FontWeight.BOLD),
+                            IconButton(
+                                icon=Icons.CLOSE,
+                                style=ButtonStyle(shape=CircleBorder()),
+                                on_click=lambda _: self.page.close_dialog(),
+                            ),
+                        ],
+                        alignment=MainAxisAlignment.SPACE_BETWEEN,
+                    ),
+                    Divider(), 
+                    Text(
+                        "Mallouka Motors is a premier Auto Parts Store, specializing in sourcing and providing top-tier used car motors directly from Europe. Our unwavering commitment is to empower our clients to transform their vehicles, taking them from a state of weakness to unparalleled strength and performance. We meticulously select each motor, ensuring exceptional quality and reliability. At Mallouka Motors, we don't just sell parts; we deliver the potential for your car to become the best version of itself.",
+                        text_align=TextAlign.JUSTIFY,
+                    ),
+                    Row(
+                        [
+                            IconButton(
+                                icon=Icons.MAIL,
+                                style=ButtonStyle(
+                                    shape=CircleBorder(),
+                                    padding=padding.all(10),
+                                ),
+                                on_click=lambda _: self.page.launch_url("mailto:malloukamotors21@gmail.com"),
+                            ),
+                            IconButton(
+                                content=Image(
+                                    src="./icons/instagram.svg",
+                                    color=Colors.PRIMARY,
+                                ),
+                                style=ButtonStyle(
+                                    color=Colors.PRIMARY,
+                                    shape=CircleBorder(),
+                                    padding=padding.all(10),
+                                ),
+                                on_click=lambda _: self.page.launch_url("https://www.instagram.com/allo.casse.auto.tn/"),
+                            ),
+                            IconButton(
+                                icon=Icons.TIKTOK,
+                                style=ButtonStyle(
+                                    shape=CircleBorder(),
+                                    padding=padding.all(10),
+                                ),
+                                on_click=lambda _: self.page.launch_url("https://www.tiktok.com/@allo.casse.auto.tn"),
+                            ),
+                        ],
+                        alignment=MainAxisAlignment.CENTER,
+                    ),
+                ],
+                tight=True,
+            ),
+            adaptive=True,
+            on_dismiss=lambda e: print("Dialog dismissed!"),
+        )
+        self.page.dialog = dlg
+        dlg.open = True
+        self.page.update()
+    
+    def make_frame(self):
+        self.settings_frame = Container(
             content=Column(
                 controls=[
                     Container(
-                        content=Column(
-                            controls=[
-                                Text("Settings", size=24, text_align='center', weight='bold'),
-                            ],
-                            horizontal_alignment=CrossAxisAlignment.CENTER,
-                        ),
-                        margin=margin.only(top=20, bottom=20),
+                        content=Text("Settings", size=24, text_align='center', weight='bold'),
+                        margin=margin.only(top=20, bottom=10),
                         alignment=alignment.center,
                     ),
+                    self.switcher,
+                    ElevatedButton(
+                        content=Row(
+                            [
+                                Icon(icons.INFO_OUTLINE),
+                                Text("About Us"),
+                            ],
+                            alignment=MainAxisAlignment.CENTER,
+                            tight=True,
+                        ),
+                        on_click=self.open_about_dialog,
+                    ),
+                    Divider(height=50),
+                    self.page.bottom_appbar,
                 ],
                 horizontal_alignment=CrossAxisAlignment.CENTER,
+                expand=True,
             ),
+            padding=padding.all(20),
             expand=True,
         )
-        
         return self.settings_frame
+
 
     def update(self) -> None:
         if self.is_mounted():
